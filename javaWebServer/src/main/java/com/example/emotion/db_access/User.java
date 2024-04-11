7f80c899eaf47b2797d675240242ac9f114717a1
Bartek Chrzanowski <bartlomiej.chrzanowski02@wp.pl>
2024-04-11 14:19:30 +0200

    First User class addition
    Added 2 constructors:
    - with all parameters
    - without any parameters
    
    Added a bunch of fields related to User
    
    Getters and setters established :100:
    
    Implemented starter-validation:3.2.4 in dependencies (build.gradle)

--- javaWebServer/src/main/java/com/example/emotion/db_access/User.java
+++ javaWebServer/src/main/java/com/example/emotion/db_access/User.java
@@ -1,4 +1,103 @@
 package com.example.emotion.db_access;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class User {
+    private long userId;
+    private String email;
+    private String password;
+    private String name;
+    private String surname;
+    private Integer birthYear;
+    private String sex;
+    private String placeOfResidence;
+    private String additionalInformation;
+
+    public User() {}
+
+    public User(long userId, String email, String password, String name, String surname, Integer birthYear, String sex, String placeOfResidence, String additionalInformation) {
+        this.userId = userId;
+        this.email = email;
+        this.password = password;
+        this.name = name;
+        this.surname = surname;
+        this.birthYear = birthYear;
+        this.sex = sex;
+        this.placeOfResidence = placeOfResidence;
+        this.additionalInformation = additionalInformation;
+    }
+
+    public long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(long userId) {
+        this.userId = userId;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public Integer getBirthYear() {
+        return birthYear;
+    }
+
+    public void setBirthYear(Integer birthYear) {
+        this.birthYear = birthYear;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getPlaceOfResidence() {
+        return placeOfResidence;
+    }
+
+    public void setPlaceOfResidence(String placeOfResidence) {
+        this.placeOfResidence = placeOfResidence;
+    }
+
+    public String getAdditionalInformation() {
+        return additionalInformation;
+    }
+
+    public void setAdditionalInformation(String additionalInformation) {
+        this.additionalInformation = additionalInformation;
+    }
+
 }
