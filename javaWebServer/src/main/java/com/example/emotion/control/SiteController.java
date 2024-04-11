8a4ac8999c058045353a3702d78dfa396456fd12
igorb <igor.bialas@gmail.com>
2024-04-11 13:02:52 +0200

    Hello page added.

--- javaWebServer/src/main/java/com/example/emotion/control/SiteController.java
+++ javaWebServer/src/main/java/com/example/emotion/control/SiteController.java
@@ -7,5 +7,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("")
 public class SiteController {
-
+    @GetMapping("/hello")
+    public String helloSite() {
+        return "hello";
+    }
 }
