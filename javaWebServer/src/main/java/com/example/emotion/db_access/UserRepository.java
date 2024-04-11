16c8160705ea4c100ac0816e1ff3a2f1762db489
IgorBia <igor.bialas@gmail.com>
2024-04-12 01:39:41 +0200

    LOGOWANIE!!!!

--- javaWebServer/src/main/java/com/example/emotion/db_access/UserRepository.java
+++ javaWebServer/src/main/java/com/example/emotion/db_access/UserRepository.java
@@ -1,6 +1,7 @@
 package com.example.emotion.db_access;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByEmail(String email);
